7babc0f2f4abc325e887a93bffd4075dc41b5944
Taylor <taylor.woodcock@outlook.com>
2023-11-30 21:25:43 +0000

    feat: add `main.go` placeholder

---
+++ cmd/awaik/main.go
@@ -0,0 +1,15 @@
+package main
+
+func init() {}
+
+// main is the entrypoint to the program
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+// run runs the application with error handling
+func run() error {
+	return nil
+}
